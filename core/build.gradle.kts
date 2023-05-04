a698647869b348435f3dcf578bc7c8d2729f96bf
Hussien <hussien.fahmy98@gmail.com>
2023-05-04 21:17:14 +0300

    Fix R class issue when reference from another module by adding unique namespace to core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,6 +2,14 @@ apply {
     from("$rootDir/base-module.gradle")
 }
 
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "${ProjectConfig.appId}.core"
+}
+
 dependencies {
 
 }
