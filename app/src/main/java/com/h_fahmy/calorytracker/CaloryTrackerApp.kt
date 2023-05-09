c8f7d03cb4fa98ee739a50837131c514d10a9c84
Hussien <hussien.fahmy98@gmail.com>
2023-05-09 16:52:32 +0300

    Setup Hilt in app module

---
+++ app/src/main/java/com/h_fahmy/calorytracker/CaloryTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.h_fahmy.calorytracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CaloryTrackerApp : Application()
---
+++ app/src/main/java/com/h_fahmy/calorytracker/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.h_fahmy.calorytracker.di
+
+import android.content.Context
+import com.h_fahmy.core.data.preferences.UserInfoDataStore
+import com.h_fahmy.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePresences(@ApplicationContext context: Context): Preferences =
+        UserInfoDataStore.create(context)
+
+}
