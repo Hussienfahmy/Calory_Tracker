b946f1e18f946ca1129001a474147f4d98dab5fe
Hussien <hussien.fahmy98@gmail.com>
2023-05-28 14:11:18 +0300

    Move Providing preferences from app module to core module

--- core/src/main/java/com/h_fahmy/core/di/Module.kt
+++ core/src/main/java/com/h_fahmy/core/di/Module.kt
@@ -1,10 +1,14 @@
 package com.h_fahmy.core.di
 
+import android.content.Context
+import com.h_fahmy.core.data.preferences.UserInfoDataStore
+import com.h_fahmy.core.domain.preferences.Preferences
 import com.h_fahmy.core.domain.use_case.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
@@ -15,4 +19,9 @@ object Module {
     @Provides
     @ViewModelScoped
     fun provideFilterOutDigitsUseCase() = FilterOutDigits()
+
+    @Provides
+    @ViewModelScoped
+    fun providePresences(@ApplicationContext context: Context): Preferences =
+        UserInfoDataStore.create(context)
 }
